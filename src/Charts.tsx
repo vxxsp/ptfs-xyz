860c040aec56b068a14bfff39f5ea6c0ec2c814a
HotDog640 <Hotdog640derp21@gmail.com>
2021-03-31 14:33:34 +0100

    Tooltips + experimental rotate feature

--- src/Charts.tsx
+++ src/Charts.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Alert, Button, Col, Container, Modal, Row } from "react-bootstrap";
+import { Alert, Button, Col, Container, Modal, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 
 const Charts = () =>{
   const [ModalShow, setModalShow] = useState(false);
+  const [rotated, rotate] = useState(false);
   const closeModal = () => setModalShow(false);
   const showModal = () => setModalShow(true);
   const [chart, setChart] = useState("");
@@ -55,6 +56,23 @@ const Charts = () =>{
       setChart(codes[newIndex]);
   };
 
+  const ButtonTooltip = (props: {
+    onClick: () => void,
+    text: string,
+    tooltip: string
+    nodelay?: boolean
+  }) => {
+    return (
+      <OverlayTrigger
+        placement="top"
+        delay={{ show: props.nodelay ? 0 : 500, hide: 0 }}
+        overlay={<Tooltip id={props.tooltip}>{props.tooltip}</Tooltip>}
+      >
+        <Button onClick={props.onClick}>{props.text}</Button>
+      </OverlayTrigger>
+    )  
+  };
+
   return (
     <>
       <Container>
@@ -86,13 +104,15 @@ const Charts = () =>{
               src={`/charts/${chart} Ground Chart.png`}
               alt={`Airport ground chart for the airport ${chart}`}
               width="100%"
+              style={{transform: rotated ? "rotate(90deg)" : undefined}}
             />
           </a>
         </Modal.Body>
 
         <Modal.Footer>
-          <Button onClick={() => {nextChart(false)}}>&lt;</Button>
-          <Button onClick={() => {nextChart(true)}}>&gt;</Button>
+          <ButtonTooltip onClick={() => {nextChart(false)}} text="<" tooltip="Previous Chart" />
+          <ButtonTooltip onClick={() => {rotate(!rotated)}} text="⟳" tooltip="Rotate Chart (Experimental)" nodelay />
+          <ButtonTooltip onClick={() => {nextChart(true)}} text=">" tooltip="Next Chart" />
           <Button variant="secondary" onClick={() => {window.open("link")}}>Open Image in New Tab</Button>
         </Modal.Footer>
       </Modal>
