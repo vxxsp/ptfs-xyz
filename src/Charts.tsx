b017c73662c9b8ab8b1cd0b3398013f6a9a349bb
HotDog640 <Hotdog640derp21@gmail.com>
2021-04-01 23:01:08 +0100

    made the grid reactive | thanks fireship.io

--- src/Charts.tsx
+++ src/Charts.tsx
@@ -1,76 +1,51 @@
-import { useState } from "react";
-import { Alert, Button, Col, Container, Modal, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+import { useState } from 'react';
+import { Alert, Button, Container, Modal } from 'react-bootstrap';
+import { ButtonTooltip } from './components/ButtonTooltip';
+import './charts.css';
 
-const Charts = () =>{
+const Charts = () => {
   const [ModalShow, setModalShow] = useState(false);
-  const [rotated, rotate] = useState(false);
   const closeModal = () => setModalShow(false);
   const showModal = () => setModalShow(true);
-  const [chart, setChart] = useState("");
+  const [rotated, rotate] = useState(false);
+  const [chart, setChart] = useState('');
 
   const codes = [
-    'BIGK', 'KBLT', 'KGAR',
-    'KMLR', 'KRFD', 'KTRC',
-    'LCHS', 'LCLK', 'LCPH',
-    'LCPR', 'LGSK', 'LJIO',
-    'LJNF', 'LJSC', 'TFFJ',
-    'TFFU', 'YPLK', 'YPPH'
+    'BIGK',
+    'KBLT',
+    'KGAR',
+    'KMLR',
+    'KRFD',
+    'KTRC',
+    'LCHS',
+    'LCLK',
+    'LCPH',
+    'LCPR',
+    'LGSK',
+    'LJIO',
+    'LJNF',
+    'LJSC',
+    'TFFJ',
+    'TFFU',
+    'YPLK',
+    'YPPH',
   ];
 
-  const cols = codes.map((code) =>
-    <Col key={code}>
-      <a href="#/" onClick={() => {
-        setChart(code);
-        showModal();
-      }}>
-        <img 
-          src={`/charts/preview/${code} Ground Chart.png`}
-          width="100%"
-          alt={`Airport ground chart for the airport ${code}`}
-        />
-      </a>
-    </Col>
-  );
-
-  const genRows = () => {
-    const rows = [];
-    for (let i = 0; i <= codes.length; i +=3) {
-      rows.push(
-        <>
-          <Row>
-            {cols[(i)]}
-            {cols[(i + 1)]}
-            {cols[(i + 2)]}
-          </Row>
-          <br />
-        </>
-      )
-    }
-    return rows;
-  };
-
-  const nextChart = (next: boolean) => {
+  const nChart = (next = true) => {
     const index = codes.indexOf(chart);
-    const newIndex = index + (next ? 1 : -1);
-    if (newIndex >= 0 && newIndex < codes.length)
-      setChart(codes[newIndex]);
+    const length = codes.length;
+    let newIndex = index + (next ? 1 : -1);
+
+    if (newIndex < 0) newIndex = length - 1;
+    else if (newIndex >= length) newIndex = 0;
+
+    setChart(codes[newIndex]);
   };
 
-  const ButtonTooltip = (props: {
-    onClick: () => void,
-    text: string,
-    tooltip: string
-    nodelay?: boolean
-  }) => {
-    return (
-      <OverlayTrigger
-        placement="top"
-        delay={{ show: props.nodelay ? 0 : 500, hide: 0 }}
-        overlay={<Tooltip id={props.tooltip}>{props.tooltip}</Tooltip>}
-      >
-        <Button onClick={props.onClick}>{props.text}</Button>
-      </OverlayTrigger>
-    )  
+  const pChart = () => nChart(false);
+
+  const openChart = () => {
+    window.open(`/charts/${chart} Ground Chart.png`);
   };
 
   return (
@@ -78,42 +53,62 @@ const Charts = () =>{
       <Container>
         <Alert variant="dark">
           <h4>These charts are not released yet!</h4>
-          <p>This is just a beta preview of the charts for feedback. Please message
-          HotDog#6400 on Discord if you have any feedback or notice any mistakes
-          on these charts. Thank you.</p>
+          <p>
+            This is just a beta preview of the charts for feedback. Please
+            message HotDog#6400 on Discord if you have any feedback or notice
+            any mistakes on these charts. Thank you.
+          </p>
           <hr />
           The taxiway lettering not matching the ones in game is not a mistake,
           it's a proposal.
         </Alert>
-        {genRows()}
+        <div className="chart-grid">
+          {codes.map((code) => (
+            <a
+              href="#/"
+              onClick={() => {
+                setChart(code);
+                showModal();
+              }}
+            >
+              <img
+                src={`/charts/preview/${code} Ground Chart.png`}
+                width="100%"
+                alt={`Airport ground chart for the airport ${code}`}
+              />
+            </a>
+          ))}
+        </div>
       </Container>
-
-      <Modal
-        show={ModalShow}
-        onHide={closeModal}
-        size="lg"
-        centered
-      >
+      <Modal show={ModalShow} onHide={closeModal} size="lg" centered>
         <Modal.Header closeButton>
           <Modal.Title>{chart}</Modal.Title>
         </Modal.Header>
-
         <Modal.Body>
           <a href={`/charts/${chart} Ground Chart.png`}>
             <img
               src={`/charts/${chart} Ground Chart.png`}
               alt={`Airport ground chart for the airport ${chart}`}
               width="100%"
-              style={{transform: rotated ? "rotate(90deg)" : undefined}}
+              style={{ transform: rotated ? 'rotate(90deg)' : undefined }}
             />
           </a>
         </Modal.Body>
-
         <Modal.Footer>
-          <ButtonTooltip onClick={() => {nextChart(false)}} text="<" tooltip="Previous Chart" />
-          <ButtonTooltip onClick={() => {rotate(!rotated)}} text="⟳" tooltip="Rotate Chart (Experimental)" nodelay />
-          <ButtonTooltip onClick={() => {nextChart(true)}} text=">" tooltip="Next Chart" />
-          <Button variant="secondary" onClick={() => {window.open("link")}}>Open Image in New Tab</Button>
+          <ButtonTooltip onClick={nChart} text="<" tooltip="Previous Chart" />
+          <ButtonTooltip
+            onClick={() => {
+              rotate(!rotated);
+            }}
+            text="⟳"
+            tooltip="Rotate Chart (Experimental)"
+            nodelay
+            className="hide-on-portrait"
+          />
+          <ButtonTooltip onClick={pChart} text=">" tooltip="Next Chart" />
+          <Button variant="secondary" onClick={openChart}>
+            Open Image in New Tab
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
---
+++ src/components/ButtonTooltip.tsx
@@ -0,0 +1,29 @@
+import { OverlayTrigger, Tooltip, Button } from 'react-bootstrap';
+
+const ButtonTooltip = ({
+  onClick,
+  className,
+  text,
+  tooltip,
+  nodelay,
+}: {
+  onClick: () => void;
+  className?: string;
+  text: string;
+  tooltip: string;
+  nodelay?: boolean;
+}) => {
+  return (
+    <OverlayTrigger
+      placement="top"
+      delay={{ show: nodelay ? 0 : 500, hide: 0 }}
+      overlay={<Tooltip id={tooltip}>{tooltip}</Tooltip>}
+    >
+      <Button onClick={onClick} className={className}>
+        {text}
+      </Button>
+    </OverlayTrigger>
+  );
+};
+
+export { ButtonTooltip };
