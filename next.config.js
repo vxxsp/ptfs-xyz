62ca82549cc1376138c28cdfc9e3c151f8b93c4a
HotDog640 <Hotdog640derp21@gmail.com>
2021-07-05 21:43:48 +0100

    netlify stuff

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
+  target: 'serverless',
   pwa: {
     dest: 'public',
     runtimeCaching,
